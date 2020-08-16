bbf4eb09d9d9bac2efcfa4e6d5bcf82e6f0a89b2
wangshichao <615554360@qq.com>
2020-08-16 17:46:37 +0800

    fix cherry conflict

--- main.c
+++ main.c
@@ -4,12 +4,8 @@ int main()
 {
 
     printf("hello world in newdev");
-<<<<<<< HEAD
-    printf("we still need sometime");
-=======
+    printf("we still need sometime\n");
     printf("fix bug");
->>>>>>> 4694354... fix bug
-
     printf("%d",test_in_main);
     int i=0;
     for(i=0;i<98;i++)
