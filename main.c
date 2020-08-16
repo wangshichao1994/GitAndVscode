bf23d9d0d538f7638d5751d08d97902944f73746
wangshichao <615554360@qq.com>
2020-08-16 10:34:08 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include"main.h"
+int main()
+{
+    printf("hello world");
+    printf("%d",test_in_main);
+    int i=0;
+    for(i=0;i<100;i++)
+    {
+        printf("%d\n",i);
+    }
+    getchar();
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+#endif
+
+int test_in_main=0;
